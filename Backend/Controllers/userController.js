3b09e6d3502a7a9bee2c869903e4c8667cf4180d
Adediran Kehinde <kennydemola2@gmail.com>
2023-02-07 02:31:07 +0100

    added get controller for the user

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -3,7 +3,6 @@ const db = require('../models')
 const User = db.users
 
 async function addUser(req,res,next){
-
 userInfo = req.body
 try{
 const user = await User.create(userInfo)
@@ -15,7 +14,37 @@ catch(err){
 }
 }
 
+async function getAllUsers(req,res,next){
+   const userInfo = req.body
+    try{
+    const users = await User.findAll({attributes:['first_name','email']})
+    res.status(201).json(users)
+    }
+    catch(err){
+        console.log(err)
+        next(err)
+    }
+    }
+
+    async function getUserByEmail(req,res,next){
+        const userEmail = req.body.email
+        // `${userEmail}`
+         try{
+        const user = await User.findOne({where:{email:userEmail}})
+        // const users = await User.findAll()
+         res.status(201).json(user)
+         }
+         catch(err){
+             console.log(err)
+             next(err)
+         }
+         }
+
+
+
 
 module.exports ={
-    addUser
+    addUser,
+    getAllUsers,
+    getUserByEmail
 }
--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -4,5 +4,7 @@ const userController = require('../Controllers/userController');
 const userRouter = express.Router()
 
 userRouter.post('/', userController.addUser)
+userRouter.get('/', userController.getAllUsers)
+userRouter.get('/email',userController.getUserByEmail)
 
 module.exports = userRouter
