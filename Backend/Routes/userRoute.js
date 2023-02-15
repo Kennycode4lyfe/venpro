92e695899cde7b9795a5251f5edc63778d41ada0
Adediran Kehinde <kennydemola2@gmail.com>
2023-02-15 01:23:58 +0100

    user already receiving email

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../Controllers/userController');
-
+const mail = require('../middleware/mail')
 const userRouter = express.Router()
 
-userRouter.post('/', userController.addUser)
+
+userRouter.post('/',mail, userController.addUser)
 userRouter.get('/', userController.getAllUsers)
 userRouter.get('/index/:id',userController.getUserById)
 
--- Backend/middleware/mail.js
+++ Backend/middleware/mail.js
@@ -1,16 +1,42 @@
 const nodemailer = require('nodemailer')
-module.exports=(req,res)=>{
-let transporter = nodemailer.createTransport({service:'gmail',
+require('dotenv').config()
+
+
+const pass = process.env.GM_PASS
+module.exports= async(req,res,next)=>{
+let transporter = await nodemailer.createTransport({service:'gmail',
 auth:{
-    user:'',
-    pass:''
+    user:'kennydemola2@gmail.com',
+    pass:pass
 }})
 
+const output = `
+<p>You have a new contact request</p>
+<h3>Contact Details</h3>
+<ul>  
+  <li>Name: ${req.body.first_name}</li>
+  <li>Company: ${req.body.last_name}</li>
+</ul>
+<h3>Message</h3>
+<p>Welcome to venpro</p>
+`;
+
+
 let mailOptions = {
-    from: '"Contact" <kennydemola2@gmail.com>',
-    to: req.email,
+    from: '"Adediran Kehinde" <kennydemola2@gmail.com>',
+    to: req.body.email,
     subject: 'verify your email',
-    text:'',
+    text:`This is your one time password 7634` ,
     html:output
 }
+
+await transporter.sendMail(mailOptions,(err,info)=>{
+    if(err){
+    console.log(err)
+    return err
+    }
+    next()
+})
+
 }
+
